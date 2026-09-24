8becb9a942ae4372d1c6eff660fec512dc7fde6f
J7V00 <bilal@aljalh.com>
2026-09-24 16:19:36 +0300

    Fix service worker paths for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,37 @@
-const CACHE_NAME = "coding-master-v1";
+const CACHE_NAME = "coding-master-v2";
 
 const FILES_TO_CACHE = [
-    "/",
-    "/html/index.html",
-    "/html/about.html",
-    "/html/contact.html",
-    "/html/download.html",
-    "/manifest.json",
-    "/assets/logo.png"
+    "./",
+    "./index.html",
+    "./about.html",
+    "./contact.html",
+    "./download.html",
+    "./manifest.json",
+    "./assets/logo.png"
 ];
 
-
 self.addEventListener("install", event => {
-
     event.waitUntil(
-
-        caches.open(CACHE_NAME).then(cache => {
-
-            return cache.addAll(FILES_TO_CACHE);
-
-        })
-
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
-
     self.skipWaiting();
-
 });
 
-
 self.addEventListener("activate", event => {
-
     event.waitUntil(
-
-        caches.keys().then(keys => {
-
-            return Promise.all(
-
+        caches.keys().then(keys =>
+            Promise.all(
                 keys
                     .filter(key => key !== CACHE_NAME)
                     .map(key => caches.delete(key))
-
-            );
-
-        })
-
+            )
+        )
     );
-
     self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
     event.respondWith(
-
-        fetch(event.request)
-            .catch(() => caches.match(event.request))
-
+        fetch(event.request).catch(() => caches.match(event.request))
     );
-
 });
